7c59eb6c5cc3741a204b4126e12e2511b373d493
Goju <41age666@gmail.com>
2022-09-24 08:02:01 +0300

    hw8

---
+++ Program.cs
@@ -0,0 +1,228 @@
+﻿/*
+// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+
+void SelectionSort(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int count = i; count < arr.GetLength(1) - j - 1; count++)
+            {
+                if (arr[i, count] < arr[i, count + 1])
+                {
+                  (arr[i, count], arr[i, count + 1]) = (arr[i, count + 1], arr[i, count]);
+                }
+            }            
+        }
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 0, 10);
+Print(arr_1);
+SelectionSort(arr_1);
+Print(arr_1);
+*/
+
+/*
+// Задача 2: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт 
+// номер строки с наименьшей суммой элементов: 1 строка
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for(int i = 0; i < row_size; i++)
+    {
+        for(int j = 0; j < column_size; j++)
+        {
+            Console.Write($" {arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for(int i = 0; i < row; i++)
+    {
+        for(int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }        
+    }
+    return arr;    
+}
+
+int [] MinRowSum (int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+    int [] RowSum = new int [row_size];
+
+    for (int i = 0; i < row_size; i++)
+    {
+        int sum = 0;
+        for (int j = 0; j < column_size; j++)
+        {
+            sum = sum + arr [i, j];
+            RowSum[i] = sum;
+            
+        }
+    }
+    return RowSum;
+}
+
+void RowString (int [] arr)
+{
+    int PosMin = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] < arr [PosMin])
+        PosMin = i;
+    }
+    Console.WriteLine ($"Строка с наименьшей суммой элементов {PosMin+1} ");
+}
+
+void NewArray (int [] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write ($"{arr[i]} ");
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row, column, 0, 100);
+Print(arr_1);
+
+int [] arr2 = MinRowSum(arr_1);
+Console.WriteLine("Суммы строк");
+NewArray(arr2);
+Console.WriteLine();
+
+RowString(arr2);
+*/
+
+/*
+// 3. Задайте две матрицы. Напишите программу,
+//    которая будет находить произведение двух матриц.
+
+
+void Print(int[,] arr)
+{
+    int row_size = arr.GetLength(0);
+    int column_size = arr.GetLength(1);
+
+    for (int i = 0; i < row_size; i++)
+    {
+        for (int j = 0; j < column_size; j++)
+            Console.Write($" {arr[i, j],3} ");
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] MassNums(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+        for (int j = 0; j < column; j++)
+            arr[i, j] = new Random().Next(from, to);
+    return arr;
+}
+
+int[,] MatrixProduct(int[,] arr_first, int[,] arr_second)
+{
+    int row_size = arr_first.GetLength(0);
+    int column_size = arr_first.GetLength(1);
+    int[,] pr_matrix = new int[row_size, column_size];
+
+    if (row_size != arr_second.GetLength(0) || column_size != arr_second.GetLength(1)) return pr_matrix;
+    
+    for (int i = 0; i < row_size; i++)       
+        for (int j = 0; j < column_size; j++)
+            pr_matrix[i, j] = arr_first[i, j] * arr_second[i, j];            
+    return pr_matrix;
+}
+
+
+Console.Write("Enter the number of rows 1: ");
+int row_1 = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns 1: ");
+int column_1 = int.Parse(Console.ReadLine());
+
+Console.Write("Enter the number of rows 2: ");
+int row_2 = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns 2: ");
+int column_2 = int.Parse(Console.ReadLine());
+
+int[,] arr_1 = MassNums(row_1, column_1, 0, 5);
+Print(arr_1);
+int[,] arr_2 = MassNums(row_2, column_2, 0, 5);
+Print(arr_2);
+
+int[,] res_matrix = MatrixProduct(arr_1, arr_2);
+Print(res_matrix);
+*/
+
+/*
